e8032abe9c0caf272f6e1e60ea277567a7b95464
Thiago <thiagoedusan5.11@outlook.com>
2022-08-19 17:57:44 -0300

    feat: foi criado o model e a migration referente a tabela users

---
+++ app/backend/src/database/models/users.ts
@@ -0,0 +1,39 @@
+import { Model, STRING, INTEGER } from 'sequelize';
+import db from '.';
+
+class users extends Model {
+  public id!: number;
+  public usarname!: string;
+  public role!: string;
+  public email!: string;
+  public password!: string;
+}
+
+users.init({
+  id: {
+    type: INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+    allowNull: false,
+  },
+  usarname: {
+    type: STRING,
+    allowNull: false,
+  },
+  role: {
+    type: STRING,
+    allowNull: false,
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+  },
+  password: {
+    type: STRING,
+    allowNull: false,
+  },
+}, {
+  sequelize: db,
+  modelName: 'users',
+  underscored: false,
+});
