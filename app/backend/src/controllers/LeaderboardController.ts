7b1eb71dcec96939bf39d14ae2743f5cccd9ba3a
Thiago <thiagoedusan5.11@outlook.com>
2022-08-31 21:59:24 -0300

    test

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -5,11 +5,14 @@ export default class LeaderboardController {
   constructor(private leaderboardServices: LeaderboardServices = new LeaderboardServices()) {}
 
   public homeTeamRankings = async (req: Request, res: Response): Promise<Response> => {
-    try {
+    /* try {
       const result = await this.leaderboardServices.homeTeamRankings();
       return res.status(200).send(result);
     } catch (err) {
       return res.status(500).send(err);
-    }
+    } */
+
+    const result = await this.leaderboardServices.homeTeamRankings();
+    return res.status(200).send(result);
   };
 }
--- app/backend/src/services/LeaderboardServices.ts
+++ app/backend/src/services/LeaderboardServices.ts
@@ -4,12 +4,14 @@ import Team from '../classes/GenericTeam';
 import { TypeAllMatche, TypeTeams, TypeClasseTeam } from '../interfaces/interfaces';
 
 export default class LeaderboardServices {
-  public homeTeamRankings = async (): Promise<Array<TypeClasseTeam>> => {
+  // eslint-disable-next-line max-lines-per-function
+  public homeTeamRankings = async ()/* : Promise<Array<TypeClasseTeam>> */ => {
     const teams: Array<TypeTeams> = await team.findAll();
     const matches: Array<TypeAllMatche> = await match.findAll();
     const leaderboard: Array<TypeClasseTeam> = [];
+    const result: Array<TypeClasseTeam> = [];
 
     teams.forEach((t) => leaderboard.push(new Team(t, matches)));
-    return leaderboard;
+    return result;
   };
 }
