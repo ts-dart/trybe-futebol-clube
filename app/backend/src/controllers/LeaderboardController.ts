696a6f6cc1ceab132efbcf7e6384803ca785a9d9
Thiago <thiagoedusan5.11@outlook.com>
2022-09-01 00:22:44 -0300

    fix: corrigindo bugs na rota "/leaderboard/home"

--- app/backend/src/controllers/LeaderboardController.ts
+++ app/backend/src/controllers/LeaderboardController.ts
@@ -5,14 +5,11 @@ export default class LeaderboardController {
   constructor(private leaderboardServices: LeaderboardServices = new LeaderboardServices()) {}
 
   public homeTeamRankings = async (req: Request, res: Response): Promise<Response> => {
-    /* try {
+    try {
       const result = await this.leaderboardServices.homeTeamRankings();
       return res.status(200).send(result);
     } catch (err) {
       return res.status(500).send(err);
-    } */
-
-    const result = await this.leaderboardServices.homeTeamRankings();
-    return res.status(200).send(result);
+    }
   };
 }
